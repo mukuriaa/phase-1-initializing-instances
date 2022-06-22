6309d60405e819db5b4a75708b67fdc8f4ade30f
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-22 00:13:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here
+class Breakfast{
+    constructor(food,drink){
+        this.food = food;
+        this.drink = drink;
+    }
+
+}
+class Lunch{
+    constructor(salad,soup,drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+
+}
+class Dinner{
+    #dessert;
+    constructor(salad,soup,entree,dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
+let breakfast = new Breakfast("eggs","juice");
+let lunch = new Lunch("side salad","broccoli cheddar soup","iced tea");
+let dinner = new Dinner("balsamic salad","consomme","filet mignon","cheesecake");
+
+console.log(breakfast);
+console.log(lunch);
+console.log(dinner);
